1e0abe51b3adf775003ce99ec30f178a6cff4dca
lucklove <gnu.crazier@gmail.com>
2024-03-20 16:51:58 +0800

    Fix top k

--- blocks/tools.py
+++ blocks/tools.py
@@ -41,7 +41,7 @@ class GoogleSearch(BaseBlock):
     def __init__(self, cx: str, key: Secret, top_k=5):
         self.cx = cx
         self.key = key
-        self.top_k = 5
+        self.top_k = top_k
 
     @span(name="search google")
     def __call__(self, text: str) -> list:
