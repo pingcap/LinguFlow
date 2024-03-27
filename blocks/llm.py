b8e2d5d511b41047e2a5ea01ee5b444f08154eca
freedeaths <register917@gmail.com>
2024-03-27 13:09:44 +0800

    add chatllm block and add missing blocks in the  file

--- blocks/llm.py
+++ blocks/llm.py
@@ -1,5 +1,7 @@
 import langchain.chains
-from langchain_core.language_models import BaseLanguageModel
+from langchain.prompts.chat import BaseChatPromptTemplate
+from langchain_core.language_models import BaseChatModel, BaseLanguageModel
+from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.prompts import StringPromptTemplate
 
 from observability import span
@@ -22,3 +24,41 @@ class LLMChain(BaseBlock):
     @span(name="LLM Chain")
     def __call__(self, text: str, **kwargs) -> str:
         return self.chain.predict(text=text, **kwargs)
+
+
+@block(name="Chat_LLM", kind="llm")
+class ChatLLMChain(BaseBlock):
+    """
+    ChatLLM is a block that uses a chat model to generate a response to a given message.
+
+    Attributes:
+        model: BaseChatModel from LangChain.
+        prompt_template_type: BaseChatPromptTemplate from LangChain which contains a system_template to use.
+    """
+
+    def __init__(
+        self, model: BaseChatModel, prompt_template_type: BaseChatPromptTemplate
+    ):
+        self.chat = model
+        self.prompt = prompt_template_type
+
+    @span(name="ChatLLM")
+    def __call__(self, messages: list, **kwargs) -> str:
+        """
+        Generate a response to a given message.
+
+        Args:
+            messages: A list of string messages.
+            **kwargs: Additional arguments to pass to the prompt template.
+        """
+        ms = []
+        for i, m in enumerate(messages):
+            if not isinstance(m, str):
+                raise TypeError(f"messages[{i}] must be a string, but got {type(m)}")
+            if i % 2 == 0:
+                ms.append(HumanMessage(content=m))
+            else:
+                ms.append(AIMessage(content=m))
+        prompt_value = self.prompt.format_prompt(messages=ms, **kwargs)
+        response = self.chat.generate_prompt([prompt_value])
+        return response.generations[0][0].text
