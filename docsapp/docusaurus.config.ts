2f6e95fefd8ecc8a56b6ad5bb59927e376f8b3ba
Suhaha <jklopsdfw@gmail.com>
2024-03-27 13:40:35 +0800

    chore(docs): doc site adjustment

--- docsapp/docusaurus.config.ts
+++ docsapp/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/LinguFlow/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -82,8 +82,8 @@ const config: Config = {
           title: "Docs",
           items: [
             {
-              label: "Tutorial",
-              to: "/docs/category/user-guide",
+              label: "User Guide",
+              to: "/docs/overview",
             },
           ],
         },
