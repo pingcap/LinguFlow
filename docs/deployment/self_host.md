92a4bd7da285638329f5dd43fa0868fa0b67a4ec
Lux <fenhuilihe@gmail.com>
2024-03-27 13:20:44 +0800

    initial user docs (#49)
    * initial user docs
    
    * add toc info
    
    * Correcting some typos
    
    * add docs tree frame
    
    * add mock link
    
    * feat(docs): init doc site
    
    * chore: remove icon
    
    * fix typos and set collapsed as true
    
    * chore: remove blocks tab
    
    * chore: docs position
    
    * tweak: docs folder
    
    * tweak: footer
    
    * tweak: editUrl
    
    * add link for docs
    
    * delete no-related image
    
    * add tracing
    
    * Update local.md
    
    * Update overview.md
    
    * Update local.md
    
    ---------
    
    Co-authored-by: Lux Lee <lux@Luxs-MacBook-Pro-2023.local>
    Co-authored-by: Suhaha <jklopsdfw@gmail.com>

---
+++ docs/deployment/self_host.md
@@ -0,0 +1,67 @@
+---
+title: Self-Host Deployment
+sidebar_label: Self-Host
+sidebar_position: 3.2
+---
+
+# Self-Host Deployment
+
+LinguFlow Server, encompassing both the API and Web UI, is open-source and can be self-hosted using Docker, offering flexibility for deployment.
+
+## Prerequisites: Database
+
+A database is essential for storing LinguFlow's business data, including applications, versions, and interactions.
+
+LinguFlow is compatible with [several databases](https://docs.sqlalchemy.org/en/20/dialects/index.html#support-levels-for-included-dialects). [TiDB Serverless](https://www.pingcap.com/tidb-serverless/) by PingCAP is recommended. It's a fully-managed, MySQL-compatible database that scales automatically and offers free quotas, making it an excellent choice for small development teams.
+
+Regardless of the database selected, ensure you have the connection string ready once the database is set up.
+
+## Configuring the Database
+
+LinguFlow leverages [alembic](https://alembic.sqlalchemy.org/en/latest/) for automatic table structure creation. However, the database must be manually created first. For TiDB Serverless, execute `create database linguflow` via the Chat2Query page on its web console. For other databases, use the respective database client to connect and execute `create database linguflow`.
+
+After creating the database, proceed with the automatic table structure creation:
+
+```sh
+# Install alembic
+pip install alembic
+
+# Initialize alembic in the LinguFlow directory
+cd LinguFlow
+alembic init alembic
+sed -i '1s|^|import model\n|' alembic/env.py
+sed -i "s|target_metadata =.*|target_metadata = model.Base.metadata|" alembic/env.py
+sed -i "s|sqlalchemy.url =.*|sqlalchemy.url = <database_url>|" alembic.ini
+alembic revision --autogenerate -m "init"
+alembic upgrade head
+```
+
+**Note**: Replace `<database_url>` with your actual database connection string in SQLAlchemy format. For TiDB Serverless, it resembles:
+
+```
+mysql+pymysql://<USER>:<PASSWORD>@<HOST>:<PORT>/linguflow?ssl_ca=/etc/ssl/certs/ca-certificates.crt&ssl_verify_cert=true&ssl_verify_identity=true
+```
+
+## Deploying the Application
+
+Before deployment, edit `docker-compose.yaml` to update the `DATABASE_URL` environment variable with your actual database URL. Then, on the production host:
+
+```sh
+docker-compose up -d
+```
+
+Access the LinguFlow page at `http://{your-public-ip}`.
+
+## How to Update
+
+To update the application:
+
+```sh
+cd LinguFlow
+docker-compose down
+git pull
+docker-compose build --no-cache
+alembic revision --autogenerate -m "update schema if any necessary database migrations"
+alembic upgrade head
+docker-compose up
+```
---
+++ docsapp/docusaurus.config.ts
@@ -0,0 +1,109 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "LinguFlow Docs",
+  tagline: "LinguFlow",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-site.example.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "PingCAP", // Usually your GitHub org/user name.
+  projectName: "LinguFlow", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          path: "../docs",
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/pingcap/LinguFlow/edit/main/docs",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "LinguFlow",
+      // logo: {
+      //   alt: "My Site Logo",
+      //   src: "img/logo.svg",
+      // },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "User Guide",
+        },
+        // {
+        //   to: "/docs/user-guide/develop/develop_builder_block",
+        //   label: "Blocks",
+        //   position: "left",
+        // },
+        {
+          href: "https://github.com/pingcap/LinguFlow",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Tutorial",
+              to: "/docs/category/user-guide",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/pingcap/LinguFlow",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} LinguFlow.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
