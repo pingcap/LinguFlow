ba5aee3cd51d8b12951b08df140cdecf4949165b
Suhaha <jklopsdfw@gmail.com>
2024-04-22 17:12:15 +0800

    tweak(ui): chat ux in list debug

--- ui/src/modules/app_builder/Toolbar/Debug/ListInteraction.tsx
+++ ui/src/modules/app_builder/Toolbar/Debug/ListInteraction.tsx
@@ -1,39 +1,81 @@
-import { ActionIcon, Button, Card, FocusTrap, Stack, Text, Textarea } from '@mantine/core'
-import { IconCircleX } from '@tabler/icons-react'
-import { getHotkeyHandler, useDisclosure } from '@mantine/hooks'
+import { ActionIcon, Box, Button, Card, FocusTrap, Group, Stack, Text, Textarea, Tooltip } from '@mantine/core'
+import { IconCircleX, IconSwitchHorizontal } from '@tabler/icons-react'
+import { getHotkeyHandler, useClickOutside, useDisclosure } from '@mantine/hooks'
+import { useEffect, useMemo, useState } from 'react'
+import { InteractionInfo } from '@api/linguflow.schemas'
 import type { InteractionProps } from '.'
 
-export const ListIntercation: React.FC<InteractionProps<string[]>> = ({ value = [], onChange }) => {
+export const ListIntercation: React.FC<InteractionProps<string[]>> = ({
+  value = [],
+  onChange,
+  onSubmit,
+  interactions
+}) => {
   const [showAddInput, { open, close }] = useDisclosure(false)
-  const handleSubmit = (e: React.FocusEvent<HTMLTextAreaElement>) => {
+  const handleChange = (e: React.FocusEvent<HTMLTextAreaElement>) => {
     if (!e.target.value) {
       return
     }
     onChange([...value, e.target.value])
     close()
   }
+  const [showChat, _setShowChat] = useState(false)
+  const handleSetShowChat = (v: React.SetStateAction<boolean>) => {
+    onChange([])
+    _setShowChat(v)
+  }
+  const handleAddChat = (t: string) => {
+    onChange([...value, t])
+  }
 
   return (
-    <Stack>
-      {value?.map((item, index) => (
-        <ListItem
-          key={index}
-          data={item}
-          onDelete={() => onChange(value.filter((_, _index) => index !== _index))}
-          onEdit={(v) => onChange([...value.slice(0, index), v, ...value.slice(index + 1)])}
-        />
-      ))}
-      {showAddInput && (
-        <FocusTrap active>
-          <Textarea size="xs" autosize onBlur={handleSubmit} onKeyDown={getHotkeyHandler([['Enter', handleSubmit]])} />
-        </FocusTrap>
-      )}
-      {!showAddInput && (
-        <Button variant="default" onClick={open} style={{ borderStyle: 'dashed' }}>
-          Add
-        </Button>
-      )}
-    </Stack>
+    <Group align="flex-start" style={{ flexWrap: 'nowrap' }}>
+      <Tooltip label={showChat ? 'Switch to list mode' : 'Switch to chat mode'}>
+        <ActionIcon
+          pos="sticky"
+          top={0}
+          left={0}
+          style={{ zIndex: 99 }}
+          variant="subtle"
+          color="gray"
+          aria-label="Switch"
+          onClick={() => handleSetShowChat((v) => !v)}
+        >
+          <IconSwitchHorizontal style={{ width: '70%', height: '70%' }} stroke={1.5} />
+        </ActionIcon>
+      </Tooltip>
+      <Stack style={{ flexGrow: 1 }}>
+        {showChat ? (
+          <ListChat interactions={interactions} value={value} onChange={handleAddChat} onSubmit={onSubmit} />
+        ) : (
+          <>
+            {value?.map((item, index) => (
+              <ListItem
+                key={index}
+                data={item}
+                onDelete={() => onChange(value.filter((_, _index) => index !== _index))}
+                onEdit={(v) => onChange([...value.slice(0, index), v, ...value.slice(index + 1)])}
+              />
+            ))}
+            {showAddInput && (
+              <FocusTrap active>
+                <Textarea
+                  size="xs"
+                  autosize
+                  onBlur={handleChange}
+                  onKeyDown={getHotkeyHandler([['Enter', handleChange]])}
+                />
+              </FocusTrap>
+            )}
+            {!showAddInput && (
+              <Button variant="default" onClick={open} style={{ borderStyle: 'dashed' }}>
+                Add
+              </Button>
+            )}
+          </>
+        )}
+      </Stack>
+    </Group>
   )
 }
 
@@ -70,5 +112,110 @@ const ListItem: React.FC<{ data: string; onDelete: () => void; onEdit: (v: strin
     <FocusTrap active>
       <Textarea size="xs" autosize onBlur={handleSubmit} onKeyDown={getHotkeyHandler([['Enter', handleSubmit]])} />
     </FocusTrap>
+  )
+}
+
+const ListChat: React.FC<{
+  value: string[]
+  interactions?: InteractionInfo[]
+  onChange: (t: string) => void
+  onSubmit: () => void
+}> = ({ value, interactions = [], onChange, onSubmit }) => {
+  const [chatInput, setChatInput] = useState('')
+  useEffect(() => {
+    if (!value.length) {
+      return
+    }
+    onChange(interactions[interactions.length - 1].output!)
+  }, [interactions])
+  const chatItems = useMemo(() => {
+    const items = [...value]
+    items.reverse()
+    return items
+  }, [value])
+  const outsideRef = useClickOutside(() => {
+    if (!chatInput) {
+      return
+    }
+    onChange(chatInput)
+    setChatInput('')
+  })
+
+  return (
+    <Stack ref={outsideRef} pos="relative" maw="100%">
+      <Box pos="sticky" top={0} left={0} style={{ zIndex: 99 }}>
+        <Textarea
+          value={chatInput}
+          onChange={(e) => setChatInput(e.currentTarget.value)}
+          onKeyDown={getHotkeyHandler([
+            [
+              'Enter',
+              (e: React.FocusEvent<HTMLTextAreaElement>) => {
+                if (!e.target.value) {
+                  return
+                }
+                onChange(e.target.value)
+                setChatInput('')
+                setTimeout(() => {
+                  onSubmit()
+                }, 500)
+              }
+            ]
+          ])}
+        />
+      </Box>
+      <Stack>
+        {chatItems.map((v, i) =>
+          (chatItems.length - i) % 2 === 0 ? <AssistantMessage key={i} msg={v} /> : <UserMessage key={i} msg={v} />
+        )}
+      </Stack>
+    </Stack>
+  )
+}
+
+const UserMessage: React.FC<{ msg: string }> = ({ msg }) => {
+  return (
+    <Card
+      maw="95%"
+      px="md"
+      py="xs"
+      fz="sm"
+      bg="blue.0"
+      c="gray.8"
+      shadow="none"
+      style={{
+        flexShrink: 0,
+        alignSelf: 'end',
+        whiteSpace: 'pre-wrap',
+        wordBreak: 'break-word'
+      }}
+      withBorder={false}
+    >
+      {msg}
+    </Card>
+  )
+}
+
+const AssistantMessage: React.FC<{ msg: string }> = ({ msg }) => {
+  return (
+    <Card
+      maw="95%"
+      px="md"
+      py="xs"
+      fz="sm"
+      bg="gray.1"
+      c="gray.8"
+      shadow="none"
+      style={{
+        position: 'relative',
+        flexShrink: 0,
+        alignSelf: 'start',
+        overflow: 'visible',
+        wordBreak: 'break-word'
+      }}
+      withBorder={false}
+    >
+      {msg}
+    </Card>
   )
 }
